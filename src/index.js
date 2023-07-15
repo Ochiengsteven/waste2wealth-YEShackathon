e88408e6665d864d28122ee82c5d7399fe8df42c
Ochiengsteven <stevenochieng6305@gmail.com>
2023-07-15 12:01:04 +0300

    set-up webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+const title = document.createElement('h3');
+title.textContent = 'Hello World!';
+
+const page = document.querySelector('body');
+page.appendChild(title);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|jpeg|png|svg|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
